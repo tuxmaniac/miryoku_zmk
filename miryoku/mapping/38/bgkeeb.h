d9aa14870be6162ff448e183696dfcb370ec98ff
Александр Кудряшов <tuxmaniacster@gmail.com>
2024-06-11 09:56:37 +0500

    Add BGKeeB board

---
+++ miryoku/mapping/38/bgkeeb.h
@@ -0,0 +1,19 @@
+#if !defined (MIRYOKU_LAYOUTMAPPING_BGKEEB)
+
+#define XXX &none
+#define PLP &kp K_PLAY_PAUSE
+
+#define MIRYOKU_LAYOUTMAPPING_CORNE( \
+     K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
+     K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
+     K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
+     N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
+) \
+K00  K01  K02  K03  K04       K05  K06  K07  K08  K09 \
+K10  K11  K12  K13  K14       K15  K16  K17  K18  K19 \
+K20  K21  K22  K23  K24       K25  K26  K27  K28  K29 \
+     PLP  K32  K33  K34       K35  K36  K37  XXX
+
+#endif
+
+#define MIRYOKU_MAPPING MIRYOKU_LAYOUTMAPPING_BGKEEB
